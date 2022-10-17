d15abe7232b202b1d5b9609a8faed2b248a03f82
L1nkiZ <guillaume.gerard2112@outlook.fr>
2022-10-17 14:42:06 +0200

    First Commit

---
+++ SecondConsoleApp/SecondConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace SecondConsoleApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Lecture d'une entrée utilisateur
+            Console.WriteLine("Hello World!");
+            Console
+                .
+                WriteLine("This is a second line");
+            //Ecriture d'une variable
+            var entree = Console.ReadLine();
+            Console.WriteLine("Vous avez écris "+entree);
+            Console.WriteLine("Fin");
+
+
+            byte octet = 42;
+            int entier = octet;
+            octet = (byte)entier;
+
+            var a1 = 4 + 4;
+            var a2 = "4" + "4";
+            var a3 = Convert.ToInt16(a2);
+
+            var a4 = int.Parse(a2);
+            */
+
+            var random = new Random();
+            var aleatoire = random.Next(1, 100);
+            var boucle = true;
+                
+            Console.WriteLine("Bienvenue !");
+
+            while (boucle)
+            {
+                Console.WriteLine("Donnez un chiffre :");
+                var entree = Console.ReadLine();
+                var entreeint = Convert.ToInt16(entree);
+
+                if (aleatoire == entreeint)
+                {
+                    Console.WriteLine("Félicitations vous avez trouvé !!");
+                    boucle = false;
+                }
+                else if (aleatoire < entreeint)
+                {
+                    Console.WriteLine("Plus petit que " + entree);
+                }
+                else
+                {
+                    Console.WriteLine("Plus grand que " + entree);
+                }
+            }
+        }
+    }
+}
