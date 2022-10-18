84b802f051687e82fd553c258e8b8cb874c9fef6
L1nkiZ <guillaume.gerard2112@outlook.fr>
2022-10-18 10:36:13 +0200

    JeuxNombreAvecFonction

---
+++ CoursCSharp/CoursCSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private int _myValue;
+        public int MyValue
+        {
+            get
+            {
+                return _myValue;
+            }
+            set
+            {
+                _myValue = value;
+            }
+        }
+
+        public int MyValue2 { get; set; }   //équivalent à ce qui est juste au dessus -> valeur "private int _myValue;" généré automatiquement
+
+        //Exemple fonction
+        static int CleanDatabase()
+        {
+            Console.WriteLine("Cleaning database since x day");
+            return 150;
+        }
+
+        static int CleanDatabase(DateTime since)
+        {
+            Console.WriteLine($"Cleaning database since {since} day");
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            const String EXEMPLE = ""; //constate, dois initialisé une valeur et la valeur ne change pas
+
+            var x1 = CleanDatabase();
+            var x2 = CleanDatabase(DateTime.Now);
+
+        }
+    }
+
+    //<type de retour> <nomFonction()
+    //{
+    //  code
+    //}
+}
---
+++ CoursCSharp/JeuxNombreAvecFonction/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace SecondConsoleApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int NeedANumber()
+            {
+                var entree_int = 0;
+                var try_int = false;
+                var entree = "";
+
+                while (try_int != true)
+                {
+                    Console.WriteLine("Donnez un chiffre entre 1 et 100 :");
+                    entree = Console.ReadLine();
+                    try_int = int.TryParse(entree, out entree_int);
+                }
+
+                return entree_int;
+
+            }
+
+            var random = new Random();
+            var aleatoire = random.Next(1, 100);
+            var tentative = 0;
+            var intEntree = 0;
+
+            Console.WriteLine("Bienvenue !");
+
+            while (aleatoire != intEntree)
+            {
+                intEntree = NeedANumber();
+
+                tentative = tentative + 1;
+
+                if (aleatoire < intEntree)
+                {
+                    Console.WriteLine("Plus petit que " + intEntree);
+                }
+                else
+                {
+                    Console.WriteLine("Plus grand que " + intEntree);
+                }
+            }
+            Console.WriteLine("Félicitations, vous avez trouvé en " + tentative + " tentative(s) !!");
+        }
+    }
+}
