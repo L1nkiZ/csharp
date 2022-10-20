765e0957924d64f875cca3e633c551b03f5cb530
L1nkiZ <guillaume.gerard2112@outlook.fr>
2022-10-20 17:04:25 +0200

    All

--- CoursADO/CoursADO/Program.cs
+++ CoursADO/CoursADO/Program.cs
@@ -6,20 +6,48 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
         const String CHAINE_CONNEXION = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=testDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        static SqlConnection connection;
+
         static void Main(string[] args)
         {
-            var guid = Guid.NewGuid;
+            Connect();
 
-            Console.WriteLine(guid);
+            var sql = "SELECT * FROM testDB";
+            SqlCommand cmd = new SqlCommand(sql);
+
+            cmd.Connection = connection;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var username = reader.GetString(1);
+                var date = reader.GetDateTime(2);
+
+                Console.WriteLine($"{id},{username},{date.ToShortDateString()}");
+            }
+
+            sql = "insert into demo (username, birthday) values '@u, @d";
+            cmd = new SqlCommand(sql);
+            cmd.Parameters.Add(new SqlParameter("@u", "Jean"));
+            cmd.Parameters.Add(new SqlParameter("@d", DateTime.Now));
+
+            Disconnect();
         }
 
         static void Connect()
         {
-            SqlConnection connexion =
+            connection =
                 new SqlConnection(CHAINE_CONNEXION);
 
-            connexion.Open();
+            connection.Open();
             Console.WriteLine("Connexion avec la BdD ouverte");
+        }
+
+        static void Disconnect()
+        {
+            connection.Close();
+            Console.WriteLine("Connexion avec la BdD fermé");
         }
     }
 }
