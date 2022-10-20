0fad04f9894644d4ab1f982465634d519a332bcd
L1nkiZ <guillaume.gerard2112@outlook.fr>
2022-10-20 16:32:53 +0200

    début bdd

--- CoursADO/CoursADO/Program.cs
+++ CoursADO/CoursADO/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        const String CHAINE_CONNEXION = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=testDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         static void Main(string[] args)
         {
             var guid = Guid.NewGuid;
 
             Console.WriteLine(guid);
+        }
+
+        static void Connect()
+        {
+            SqlConnection connexion =
+                new SqlConnection(CHAINE_CONNEXION);
+
+            connexion.Open();
+            Console.WriteLine("Connexion avec la BdD ouverte");
         }
     }
 }
